330af667d744ead2918aeee6d8874b5ad80db94a
Horno <i.palacin.a@gmail.com>
2019-04-28 17:11:05 +0200

    Reach end function left

--- AFscript.py
+++ AFscript.py
@@ -31,9 +31,9 @@ def read_file(file_name):
         line = line.rstrip('\n')
         plist.append(line)
 
-    make_ft(plist[2].split(","))
+    ft_dic = make_ft(plist[2].split(","))
 
-    a = Automata(plist[0].split(","),plist[1].split(","), plist[2].split(",") ,
+    a = Automata(plist[0].split(","),plist[1].split(","), ft_dic,
                  plist[3].split(","), plist[4].split(","), )
     return a
 
@@ -41,30 +41,10 @@ def make_ft(ft):
     ft_dic = {}
     for transition in ft:        
         if transition[0] in ft_dic:
-            ft_dic[transition[0]] = {transition[1]: ft_dic[transition[0]][transition[1]] + transition[2]}
+            ft_dic[transition[0]].append({transition[1]: transition[2]})
         else:
             ft_dic[transition[0]] = [{transition[1]: transition[2]}]
-
-        # if transition[0] in ft_dic:
-        #     ft_dic[transition[0]].append[{transition[1]: transition[2]}]
-        # else:
-        #     ft_dic[transition[0]] = [{transition[1]: transition[2]}]
-
-    print(ft_dic)
-
-
-
-
-    # for transition in ft:
-    #     if transition[0] in ft_dic:
-    #         if transition[1] in ft_dic[transition[0]]:
-    #             ft_dic[transition[0]] = ft_dic[transition[0]]+{transition[1]: transition[2]}
-    #         else:    
-    #             ft_dic[transition[0]] = {transition[1]: ft_dic[transition[0]][transition[1]] + transition[2]}
-    #     else:
-    #         ft_dic[transition[0]] = {transition[1]: transition[2]}
-
-
+    return ft_dic
 
 def check_automata(a):
     for state in a.I:
@@ -76,9 +56,41 @@ def check_automata(a):
             print(state + " no està a Q")
             exit(-2)
 
+def service_loop(a):
+    quit = 0
+    print("Insereix una paraula que vulguis provar amb l'AF definit a " + filename)
+    print("Fes \"quit\" per marxar")
+
+    while quit != 1:
+        line = sys.stdin.readline()
+        line = line.rstrip("\n")
+        if line == "quit" or line == "quit\n":
+            quit = 1
+        elif line != "\n" and line != '':
+            check_word(line, a)
+
+def check_word(line, a):
+    if(not check_alph(line, a)):
+        print("Paraula no acceptada, lletra no pertany a l'alfabet")
+        exit(-3)
+    if not reach_end(line, a):
+        print("Paraula no acceptada per l'autòmat.")
+        exit(-3)
+    print("Praula " + line + " acceptada per l'autòmat.")
+
+def reach_end(word, a):
+    return True
+
+def check_alph(word, a):
+    for letter in word:
+        if letter not in a.E:
+            return False
+    return True
+        
 if __name__ == "__main__":
     filename = get_filename()
     a = read_file(filename)
-    check_automata(a)    
+    check_automata(a)
+    service_loop(a)
 
 
